d4f5fe6af0e427be795e00302e4d4ecb55e7319b
cstom4994 <2480707737@qq.com>
2022-10-09 23:45:22 +0800

    Add MuScript test

--- Source/Engine/Game.cpp
+++ Source/Engine/Game.cpp
@@ -27,6 +27,7 @@
 
 #include <glad/glad.h>
 #include <imgui/IconsFontAwesome5.h>
+#include <string.h>
 
 #include "InEngine.h"
 
@@ -61,6 +62,121 @@ namespace MetaEngine {
 
 HostData Game::data;
 
+#include "Scripting/MuScript/Library/MuScript.hpp"
+
+MuScript::MuScriptInterpreter interp(MuScript::ModulePrivilege::allPrivilege);
+
+MuScript::Int integrationExample(MuScript::Int a, MuScript::Int b) {
+    return (a * b) + b;
+}
+
+void integrationExample() {
+
+
+#define VAR_HERE() int, float, std::string
+    using Type_Tuple = std::tuple<VAR_HERE()>;
+
+    Type_Tuple tpt;
+    std::apply([&](auto &&...args) {
+        (printf("this type %s\n", typeid(args).name()), ...);
+    },
+               tpt);
+
+
+    // Demo c++ integration
+    // Step 1: Create a function wrapper
+    auto newfunc = interp.newFunction("integrationExample", [](const MuScript::List &args) {
+        // MuScript doesn't enforce argument counts, so make sure you have enough
+        if (args.size() < 2) {
+            return std::make_shared<MuScript::Value>();
+        }
+        // Dereference arguments
+        auto a = *args[0];
+        auto b = *args[1];
+        // Coerce types
+        a.hardconvert(MuScript::Type::Int);
+        b.hardconvert(MuScript::Type::Int);
+        // Call c++ code
+        auto result = integrationExample(a.getInt(), b.getInt());
+        // Wrap and return
+        return std::make_shared<MuScript::Value>(result);
+    });
+
+    // Step 2: Call into MuScript
+    // send command into script interperereter
+    interp.readLine("i = integrationExample(4, 3);");
+
+    // get a value from the interpereter
+    auto varRef = interp.resolveVariable("i");
+
+    // or just call a function directly
+    varRef = interp.callFunctionWithArgs(newfunc, MuScript::Int(4), MuScript::Int(3));
+
+    // Setp 3: Unwrap your result
+    // if the type is known
+    int64_t i = varRef->getInt();
+    std::cout << i << "\n";
+
+    // visit style
+    std::visit([](auto &&arg) { std::cout << arg << "\n"; }, varRef->value);
+
+    // switch style
+    switch (varRef->getType()) {
+        case MuScript::Type::Int:
+            std::cout << varRef->getInt() << "\n";
+            break;
+        case MuScript::Type::Float:
+            std::cout << varRef->getFloat() << "\n";
+            break;
+        case MuScript::Type::String:
+            std::cout << varRef->getString() << "\n";
+            break;
+        default:
+            break;
+    }
+
+    // create a MuScript class from C++:
+    interp.newClass("beansClass", {{"color", std::make_shared<MuScript::Value>("white")}},
+                    // constructor is required
+                    [](MuScript::Class *classs, MuScript::ScopeRef scope, const MuScript::List &vars) {
+                        if (vars.size() > 0) {
+                            interp.resolveVariable("color", classs, scope) = vars[0];
+                        }
+                        return std::make_shared<MuScript::Value>();
+                    },
+                    // add as many functions as you want
+                    {
+                            {"changeColor", [](MuScript::Class *classs, MuScript::ScopeRef scope, const MuScript::List &vars) {
+                                 if (vars.size() > 0) {
+                                     interp.resolveVariable("color", classs, scope) = vars[0];
+                                 }
+                                 return std::make_shared<MuScript::Value>();
+                             }},
+                            {"isRipe", [](MuScript::Class *classs, MuScript::ScopeRef scope, const MuScript::List &) {
+                                 auto color = interp.resolveVariable("color", classs, scope);
+                                 if (color->getType() == MuScript::Type::String) { return std::make_shared<MuScript::Value>(color->getString() == "brown"); }
+                                 return std::make_shared<MuScript::Value>(false);
+                             }},
+                    });
+
+    // use the class
+    interp.readLine("bean = beansClass(\"grey\");");
+    interp.readLine("ripe = bean.isRipe();");
+
+    // get values from the interpereter
+    auto beanRef = interp.resolveVariable("bean");
+    auto ripeRef = interp.resolveVariable("ripe");
+
+    // read the values!
+    if (beanRef->getType() == MuScript::Type::Class && ripeRef->getType() == MuScript::Type::Int) {
+        auto colorRef = beanRef->getClass()->variables["color"];
+        if (colorRef->getType() == MuScript::Type::String) {
+            std::cout << "My bean is " << beanRef->getClass()->variables["color"]->getString() << " and it is " << (ripeRef->getBool() ? "ripe" : "unripe") << "\n";
+        }
+    }
+}
+
+
 void Game::updateMaterialSounds() {
     uint16_t waterCt = std::min(movingTiles[Materials::WATER.id], (uint16_t) 5000);
     float water = (float) waterCt / 3000;
@@ -69,6 +185,10 @@ void Game::updateMaterialSounds() {
 }
 
 int Game::init(int argc, char *argv[]) {
+
+    if (!strcmp(argv[1], "test_mu")) {
+        return interp.evaluateFile(std::string(argv[2]));
+    }
 
     //networkMode = clArgs->getBool("server") ? NetworkMode::SERVER : NetworkMode::HOST;
     networkMode = NetworkMode::HOST;
