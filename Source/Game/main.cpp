1e8707af9388fa0be262177434c14edf287f0270
KaoruXun <2480707737@qq.com>
2022-11-11 16:36:12 +0800

    Reconstruct

--- Source/Game/main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright(c) 2022, KaoruXun All rights reserved.
-
-#undef main
-
-int main(int argc, char *argv[]) {
-    // const auto game = std::make_unique<Game>();
-    // return game->init(argc, argv);
-}
