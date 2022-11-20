253ff2b1bca830ae94f9fdf943af314c80514c5b
KaoruXun <2480707737@qq.com>
2022-11-21 00:09:43 +0800

    Remove luash

--- Source/Libs/lua/luash/luash/luash.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Libs/lua/lua.hpp"
-
-#include "luash_fwd.hpp"
-#include "luash_imp.hpp"
-#include "luash_bwd.hpp"
-	
--- Source/Libs/lua/luash/meta/TypeIdentity.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <type_traits>
-
-template<class T> struct TypeIdentity : std::enable_if<true, T> {};
-template<class T> struct type_identity : TypeIdentity<T> {};
