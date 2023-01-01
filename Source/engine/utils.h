e75ac57cbd5b03db5f1cdb9a02f16079ae82b439
KaoruXun <2480707737@qq.com>
2023-01-01 21:49:02 +0800

    Regex funcs

--- Source/engine/utils.h
+++ Source/engine/utils.h
@@ -76,6 +76,23 @@
 #define UTIL_dot(u, v) ((u).x * (v).x + (u).y * (v).y + (u).z * (v).z)
 // #define norm(v) sqrt(dot(v, v))// norm = length of  vector
 
+#ifndef RE_DOT_MATCHES_NEWLINE
+/* Define to 0 if you DON'T want '.' to match '\r' + '\n' */
+#define RE_DOT_MATCHES_NEWLINE 1
+#endif
+
+/* Typedef'd pointer to get abstract datatype. */
+typedef struct regex_t *re_t;
+
+/* Compile regex string pattern to a regex_t-array. */
+re_t re_compile(const char *pattern);
+
+/* Find matches of the compiled pattern inside text. */
+int re_matchp(re_t pattern, const char *text, int *matchlength);
+
+/* Find matches of the txt pattern inside text (will compile automatically first). */
+int re_match(const char *pattern, const char *text, int *matchlength);
+
 // Trie structures and interface
 
 // Add all supported types here, as 'Trie_type'
